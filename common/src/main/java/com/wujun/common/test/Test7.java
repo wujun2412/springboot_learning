62444436a3f242e3c4d63897e5f637b4dac9ff9c
wujun <wujun@xkeshi.com>
2018-11-19 17:01:38 +0800

    提交

---
+++ common/src/main/java/com/wujun/common/test/Test7.java
@@ -0,0 +1,15 @@
+package com.wujun.common.test;
+
+/**
+ * @author wujun
+ * @description
+ * @date 18/6/12.
+ */
+public class Test7 {
+    public static void main(String[] args) {
+        String c = "123";
+        String d = "123" + "123";
+        String e = "123" + c;
+        System.out.println(d == e);
+    }
+}
