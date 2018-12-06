11ed01bed3c305a627d5b69447c80f3ec1911677
Jun.Wu <Jun.Wu@geely.com>
2018-12-06 15:57:30 +0800

    提交

---
+++ springboot_fastdfs/src/main/java/com/wujun/boot/config/FastDFSStorageConfiguration.java
@@ -0,0 +1,12 @@
+package com.wujun.boot.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+
+/**
+ * @Auther: Jun.Wu
+ * @Date: 2018-10-25 16:17
+ * @Description: FastDFS 配置类
+ */
+@ConditionalOnExpression
+public class FastDFSStorageConfiguration {
+}
